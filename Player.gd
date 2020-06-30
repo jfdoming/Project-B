e1af14b517716c743507b9bf1670589ef73c07bb
Iden Craven <iden.craven@gmail.com>
2020-06-29 21:23:18 -0600

    Sprint and slide (#34)
    * Added sprinting when player holds shift
    
    * Add sliding when player is sprinting then crouches

--- Player.gd
+++ Player.gd
@@ -10,7 +10,8 @@ export (PackedScene) var Bullet
 # Physics-related options.
 export (bool) var may_move = true
 export (bool) var obey_physics = true
-export (int) var max_run_speed = 250
+export (int) var max_walk_speed = 250
+export (int) var max_sprint_speed = 500
 export (float) var run_speed_increment_fraction = 1.0 / 10.0
 export (int) var jump_speed = 800
 export (int) var smash_speed = 1200
@@ -113,6 +114,7 @@ func calculate_velocity(delta):
 	var crouch = not freeze and Input.is_action_pressed('ui_down')
 	var fire = not freeze and Input.is_action_just_pressed("fire")
 	var walking = left != right
+	var sprinting = Input.is_action_pressed('sprint') and walking
 
 	if fire:
 		var instance = Bullet.instance()
@@ -126,7 +128,7 @@ func calculate_velocity(delta):
 	if crouch:
 		if jumping and not smashing:
 			smashing = true
-			velocity.y = smash_speed	
+			velocity.y = smash_speed
 		$HeadCollisionShape.set_disabled(true)	
 		$MustCrouchCheck.get_node("CrouchCheckCollider").set_disabled(false)
 	elif must_crouch == false:
@@ -137,8 +139,11 @@ func calculate_velocity(delta):
 		just_jumped = true
 		velocity.y = -jump_speed - jump_bonus * abs(velocity.x)	
 	if right and not left:
-		velocity.x += run_speed_increment_fraction * max_run_speed * delta * 60
-		velocity.x = clamp(velocity.x, -max_run_speed, max_run_speed)
+		velocity.x += run_speed_increment_fraction * max_walk_speed * delta * 60
+		if sprinting and not crouch:
+			velocity.x = clamp(velocity.x, -max_sprint_speed, max_sprint_speed)
+		else:
+			velocity.x = clamp(velocity.x, -max_walk_speed, max_walk_speed)
 		if direction != RIGHT:
 			direction = RIGHT
 			scale.x = -1
@@ -146,8 +151,11 @@ func calculate_velocity(delta):
 		emit_signal("flip_health_bar","RIGHT")
 		
 	if left and not right:
-		velocity.x -= run_speed_increment_fraction * max_run_speed * delta * 60
-		velocity.x = clamp(velocity.x, -max_run_speed, max_run_speed)
+		velocity.x -= run_speed_increment_fraction * max_walk_speed * delta * 60
+		if sprinting and not crouch:
+			velocity.x = clamp(velocity.x, -max_sprint_speed, max_sprint_speed)
+		else:
+			velocity.x = clamp(velocity.x, -max_walk_speed, max_walk_speed)
 		if direction != LEFT:
 			direction = LEFT
 			scale.x = -1
@@ -175,11 +183,16 @@ func calculate_velocity(delta):
 		self._show_anim($KneeAttackAnimation)
 	elif not freeze:
 		if crouch or must_crouch == true:
-			_show_anim($CrouchAnimation)
+			if sprinting:
+				_show_anim($SlideAnimation)
+			else:
+				_show_anim($CrouchAnimation)
 		elif jumping:
 			_show_anim($JumpAnimation)
-		elif walking:
+		elif walking and not sprinting:
 			_show_anim($WalkAnimation)
+		elif sprinting:
+			_show_anim($SprintAnimation)
 		else:
 			_show_anim($StandAnimation)
 
